dbd7803820c1ed0e901042f7f01813a2fc4f80d6
Desiree <diallusions@gmail.com>
2017-08-25 09:49:20 -0400

    I am not very smart at all aaaaaaaa

--- Examples/On Message Events/simple_bot.py
+++ Examples/On Message Events/simple_bot.py
@@ -1,4 +1,4 @@
-# This is a bot that does not use the commands.ext extension, meaning all commands are manually made.
+## This is a bot that does not use the commands.ext extension, meaning all commands are manually made.
 # It uses the on_message event to use these commands. First, start by importing discord.
 import discord
 
@@ -15,21 +15,21 @@ async def on_message(message):
     
     args = message.content
     args = args.replace(prefix, "")
-    args = args.split
+    args = args.split(' ')
     command = args[0]
     if message.content.startswith(prefix):
         if command == "hello":                                    # Just an example of how formats work, it replaces the number 
             if len(message.mentions) > 0:                         # of its position in the array.
-                await client.send_message("{0.mention} says hello to {1.mention}".format(message.author, message.mentions[0]))
+                await client.send_message(message.channel, "{0.mention} says hello to {1.mention}".format(message.author, message.mentions[0]))
             else:
-                await client.send_message("Hello, {0.mention}!".format(message.author))
+                await client.send_message(message.channel, "Hello, {0.mention}!".format(message.author))
                 
                 
         if command == "goodbye":
             if len(message.mentions) > 0:
-                await client.send_message("{0.mention} says goodbye to {1.mention}".format(message.author, message.mentions[0]))
+                await client.send_message(message.channel, "{0.mention} says goodbye to {1.mention}".format(message.author, message.mentions[0]))
             else:
-                await client.send_message("Goodbye, {0.mention}!".format(message.author))
+                await client.send_message(message.channel, "Goodbye, {0.mention}!".format(message.author))
                 
                 
 @client.event
